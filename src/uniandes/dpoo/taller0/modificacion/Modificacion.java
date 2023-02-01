a226f19aa7058584ac6f4d10bac9f4bc4f1f8611
n-torob <79644724+n-torob@users.noreply.github.com>
2023-02-01 00:11:59 -0500

    Initial commit

---
+++ src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,22 @@
+package uniandes.dpoo.taller0.modificacion;
+
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+
+public class Modificacion {
+    public static void main(String[] args) throws FileNotFoundException, IOException
+    {
+        System.out.println("Hola, Mundo!");
+
+        CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+        System.out.println(calc.paisConMasMedallistas());
+
+    }
+}
+
